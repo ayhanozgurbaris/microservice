5a763418edf73cbb1f444bee5fb4e6be7f7f7d84
Özgür Barış Ayhan <barisayhan01@gmail.com>
2023-10-10 13:06:57 +0300

    api and service init

---
+++ account-service/src/main/java/com/mikroservis/accountservice/api/AccountApi.java
@@ -0,0 +1,30 @@
+package com.mikroservis.accountservice.api;
+
+import com.mikroservis.accountservice.entity.Account;
+import org.springframework.http.ResponseEntity;
+
+import java.awt.print.Pageable;
+
+public class AccountApi {
+
+    public ResponseEntity<Account> get(String id){
+
+    }
+
+    public ResponseEntity<Account> save(Account account){
+
+    }
+
+    public ResponseEntity<Account> update(Account account){
+
+    }
+
+    public void delete(String id){
+
+    }
+
+    public ResponseEntity<Account> pagination(Pageable pageable){
+
+    }
+
+}
---
+++ account-service/src/main/java/com/mikroservis/accountservice/service/AccountService.java
@@ -0,0 +1,28 @@
+package com.mikroservis.accountservice.service;
+
+import com.mikroservis.accountservice.entity.Account;
+import java.awt.print.Pageable;
+
+public class AccountService {
+
+    //burada api'nin nasıl döneceği belirlenmeyeceğinden responseEntity'e gerek yok
+    public Account get(String id){
+
+    }
+
+    public Account save(Account account){
+
+    }
+
+    public Account update(Account account){
+
+    }
+
+    public void delete(String id){
+
+    }
+
+    public Account pagination(Pageable pageable){
+
+    }
+}
